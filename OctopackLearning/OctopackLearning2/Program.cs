0e23571800c1797428cf480f82442ee03309ad48
Dalmiro <dalmiro.granias@gmail.com>
2015-05-13 18:10:07 -0300

    Adding App

---
+++ OctopackLearning/OctopackLearning2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctopackLearning2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
