61afe7b044aa68b622198ccbfc1c3efbc1f6b6fb
Dalmiro <dalmiro.granias@gmail.com>
2015-08-26 11:51:40 -0300

    adding Octopack test console

---
+++ OctopusClient/OctopusClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Octopus.Client;
+
+namespace OctopusClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string OctopusURL = "http://dalmiropc";
+            string OctopusAPIKey = "API-LQ5HY1KOYHSCQKVYAK2DFK8X4";
+            var endpoint = new Octopus.Client.OctopusServerEndpoint(OctopusURL, OctopusAPIKey);
+            var repository = new Octopus.Client.OctopusRepository(endpoint);
+
+            Octopus.Client.Model.LifecycleResource Lifecycle = new Octopus.Client.Model.LifecycleResource();
+            Lifecycle.Name = "NewLifecycle";
+            
+            Octopus.Client.Model.LifecycleResource newlifecycle = repository.Lifecycles.Create(Lifecycle);
+        }
+    }
+}
---
+++ TestCloudService/HelloWorldWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HelloWorldWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
